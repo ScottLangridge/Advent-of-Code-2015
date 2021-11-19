4e3c4f864345e094faaa595ae1f510824e566a87
Scott Langridge <scott@derossi.consulting>
2021-11-19 00:56:15 +0000

    Solved day 5

--- Day-05/day05-1.py
+++ Day-05/day05-1.py
@@ -1,10 +1,34 @@
 def main(raw_input):
-    # Parse input
+    strings = raw_input.splitlines()
 
-    # Solve problem
+    nice_strings = 0
+    for str in strings:
+        if is_nice(str):
+            nice_strings += 1
 
-    # Return solution
-    return None
+    return nice_strings
+
+def is_nice(string):
+    # If it contains no naughty strings
+    naughty_substrings = ['ab', 'cd', 'pq', 'xy']
+    for sub in naughty_substrings:
+        if sub in string:
+            return False
+
+    # And it contains at least three vowels
+    vowel_count = 0
+    for vowel in 'aeiou':
+        if vowel in string:
+            vowel_count += string.count(vowel)
+    if vowel_count < 3:
+        return False
+
+    # And it contains at least one double char, it's nice.
+    for i in range(1, len(string)):
+        if string[i] == string[i - 1]:
+            return True
+
+    return False
 
 
 def get_input(filename):
--- Day-05/day05-2.py
+++ Day-05/day05-2.py
@@ -1,10 +1,37 @@
 def main(raw_input):
-    # Parse input
+    strings = raw_input.splitlines()
 
-    # Solve problem
+    nice_strings = 0
+    for str in strings:
+        if is_nice(str):
+            nice_strings += 1
 
-    # Return solution
-    return None
+    return nice_strings
+
+
+def is_nice(string):
+    # It contains at least one double letter pair
+    match_found = False
+    # For each pair of chars up to the third to last possible pair
+    for i in range(len(string) - 3):
+        pair = string[i:i + 2]
+        # If it's repeated in the rest of the string, mark a match as found
+        for ii in range(i + 2, len(string)):
+            if pair == string[ii:ii + 2]:
+                match_found = True
+                break
+        if match_found:
+            break
+
+    if match_found == False:
+        return False
+
+    # And it contains at least one double char (separated by one), it's nice.
+    for i in range(2, len(string)):
+        if string[i] == string[i - 2]:
+            return True
+
+    return False
 
 
 def get_input(filename):
