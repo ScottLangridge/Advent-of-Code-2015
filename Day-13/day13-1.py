db3c78dbf776e29321bffd5cbe538156d595795b
Scott Langridge <scott@derossi.consulting>
2021-11-19 14:25:44 +0000

    Solved day 13

--- Day-13/day13-1.py
+++ Day-13/day13-1.py
@@ -1,10 +1,37 @@
-def main(raw_input):
-    # Parse input
+from itertools import permutations
 
-    # Solve problem
+
+def main(raw_input):
+    modifiers = [parse_line(line) for line in raw_input.splitlines()]
+    modifiers_dict = {}
+    for mod in modifiers:
+        modifiers_dict[(mod[0], mod[1])] = mod[2]
+    people = set(map(lambda x: x[0], modifiers))
+
+    best_happiness = 0
+    for perm in permutations(people):
+        perm_happiness = get_happiness(perm, modifiers_dict)
+        if perm_happiness > best_happiness:
+            best_happiness = perm_happiness
 
     # Return solution
-    return None
+    return best_happiness
+
+
+def parse_line(string):
+    tokens = string.split()
+    if tokens[2] == 'gain':
+        return [tokens[0], tokens[-1].strip('.'), int(tokens[3])]
+    else:
+        return [tokens[0], tokens[-1].strip('.'), int('-' + tokens[3])]
+
+
+def get_happiness(permutation, modifiers_dict):
+    happiness = 0
+    for i in range(len(permutation)):
+        happiness += modifiers_dict[(permutation[i], permutation[(i + 1) % len(permutation)])]
+        happiness += modifiers_dict[(permutation[i], permutation[(i - 1) % len(permutation)])]
+    return happiness
 
 
 def get_input(filename):
--- Day-13/day13-2.py
+++ Day-13/day13-2.py
@@ -1,10 +1,42 @@
-def main(raw_input):
-    # Parse input
+from itertools import permutations
 
-    # Solve problem
+
+def main(raw_input):
+    modifiers = [parse_line(line) for line in raw_input.splitlines()]
+    modifiers_dict = {}
+    for mod in modifiers:
+        modifiers_dict[(mod[0], mod[1])] = mod[2]
+    people = set(map(lambda x: x[0], modifiers))
+
+    for person in people:
+        modifiers_dict[('Me', person)] = 0
+        modifiers_dict[(person, 'Me')] = 0
+    people.add('Me')
+
+    best_happiness = 0
+    for perm in permutations(people):
+        perm_happiness = get_happiness(perm, modifiers_dict)
+        if perm_happiness > best_happiness:
+            best_happiness = perm_happiness
 
     # Return solution
-    return None
+    return best_happiness
+
+
+def parse_line(string):
+    tokens = string.split()
+    if tokens[2] == 'gain':
+        return [tokens[0], tokens[-1].strip('.'), int(tokens[3])]
+    else:
+        return [tokens[0], tokens[-1].strip('.'), int('-' + tokens[3])]
+
+
+def get_happiness(permutation, modifiers_dict):
+    happiness = 0
+    for i in range(len(permutation)):
+        happiness += modifiers_dict[(permutation[i], permutation[(i + 1) % len(permutation)])]
+        happiness += modifiers_dict[(permutation[i], permutation[(i - 1) % len(permutation)])]
+    return happiness
 
 
 def get_input(filename):
