9eba765ac5fa727fc8a68a3271ef7860c6ffacf1
Scott Langridge <scott@derossi.consulting>
2021-12-03 17:07:49 +0000

    Solved day 17

--- Day-17/day17-1.py
+++ Day-17/day17-1.py
@@ -1,16 +1,19 @@
-def main(raw_input):
-    # Parse input
-
-    # Solve problem
-
-    # Return solution
-    return None
+import itertools
 
 
 def get_input(filename):
     with open(filename) as f:
         raw_input = f.read()
     return raw_input
+
+
+def main(raw_input):
+    containers = [int(x) for x in raw_input.splitlines()]
+    combs = []
+    for n in range(len(containers)):
+        combs.extend(itertools.combinations(containers, n))
+
+    return len(list(filter(lambda x: sum(x) == 150, combs)))
 
 
 if __name__ == '__main__':
